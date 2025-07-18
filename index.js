ff6644ffe2b03e4a502b873349767c58df281108
Vivek yadav <yadav.0101vivek@gamil.com>
2025-07-18 17:29:46 +0530

    second commit

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const first_name='Vivek'
 const last_name='yadav'
 
 const age='26';
+
+ function add(a,b) {
+    return a+b
+}
---
+++ utils/hello.js
@@ -0,0 +1,5 @@
+console.log("hello world");
+
+const a='aditya yadav'
+
+console.log(a);
