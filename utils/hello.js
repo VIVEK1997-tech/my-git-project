c712959b76cd6661d4b85733c4c1c28b808cbe7e
Vivek yadav <yadav.0101vivek@gamil.com>
2025-07-19 09:12:47 +0530

    new feature D is added

--- utils/hello.js
+++ utils/hello.js
@@ -9,10 +9,16 @@ console.log('new featured is added A');
 }
 
 function newFeatureB(){
-    
+
     console.log('new featured is added B');
     console.log('new featured is import auth api');
     console.log('new featured is added c');
     console.log('new featured is for email verification');
 
+}
+
+
+function newfeatureD(){
+    console.log('new feature is added D')
+    console.log('this this about the forgot passowrd')
 }
